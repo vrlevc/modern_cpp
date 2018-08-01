099bc69f5a4f2291c276bc389a18c152b7545c6c
glvlevch <viktor.levchenko@avid.com>
2018-08-01 11:02:55 +0300

    Unit Test Target with initial test case.

---
+++ modern_cpp_test/modern_cpp_test.m
@@ -0,0 +1,39 @@
+//
+//  modern_cpp_test.m
+//  modern_cpp_test
+//
+//  Created by Viktor Levchenko on 8/1/18.
+//  Copyright © 2018 LVA. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface modern_cpp_test : XCTestCase
+
+@end
+
+@implementation modern_cpp_test
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
