407442233a6aa3ce4a541cd6ad9e9ce148c20fe6
glvlevch <viktor.levchenko@avid.com>
2018-08-01 10:58:44 +0300

    Fix for initial commit

---
+++ modern_cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  modern_cpp
+//
+//  Created by Viktor Levchenko on 8/1/18.
+//  Copyright © 2018 LVA. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
