b47aac0eb9e85d53a23f6b329d874e795d31215c
glvlevch <viktor.levchenko@avid.com>
2018-09-25 09:38:30 +0300

    Initial test for pimpl

---
+++ modern_cpp_test/pimpl_test.mm
@@ -0,0 +1,21 @@
+//
+//  pimpl_test.m
+//  modern_cpp_test
+//
+//  Created by Viktor Levchenko on 9/25/18.
+//  Copyright © 2018 LVA. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface pimpl_test : XCTestCase
+
+@end
+
+@implementation pimpl_test
+
+- (void)testPimpl
+{
+}
+
+@end
